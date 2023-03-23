336b719b945b93a11a2054baf1849a3f9c2dbec6
Henrik Röschmann <henrik.roschmann@gmail.com>
2023-03-23 22:18:03 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # MaskMeister
-MaskMeister is a Visual Studio Code extension that allows you to mask and unmask code
+
+<!-- [![Version](https://vsmarketplacebadge.apphb.com/version/henrikroschmann.maskmeister.svg)](https://marketplace.visualstudio.com/items?itemName=henrikroschmann.maskmeister)
+[![Installs](https://vsmarketplacebadge.apphb.com/installs/henrikroschmann.maskmeister.svg)](https://marketplace.visualstudio.com/items?itemName=henrikroschmann.maskmeister)
+[![Ratings](https://vsmarketplacebadge.apphb.com/rating/henrikroschmann.maskmeister.svg)](https://marketplace.visualstudio.com/items?itemName=henrikroschmann.maskmeister) -->
+
+MaskMeister is a Visual Studio Code extension that allows you to mask and unmask code by replacing sensitive information with placeholders. This helps protect sensitive data when sharing code snippets with others.
+
+## Features
+
+- Mask sensitive information in your code
+- Unmask the code to reveal the original content
+- Edit the keywords used for masking and unmasking
+
+## Usage
+
+1. Install the MaskMeister extension in Visual Studio Code
+2. Open the file containing the code you want to mask
+3. To mask your code, use one of the following methods:
+   - Press `Ctrl+Shift+P` and run the `Mask Code` command
+   - Right-click and select `Mask Code` from the context menu
+4. To unmask your code, use one of the following methods:
+   - Press `Ctrl+Shift+P` and run the `Unmask Code` command
+   - Right-click and select `Unmask Code` from the context menu
+5. To edit the keywords used for masking and unmasking:
+   - Press `Ctrl+Shift+P` and run the `Edit Masked Code Keywords` command
+   - Edit the `keywords.json` file to add or remove keyword/placeholder pairs
+
+## Configuration
+
+You can configure the keywords and the path to the keywords file in your VS Code settings. Open your settings and search for "masked-code" to find the available options.
+
+## Contributing
+
+If you'd like to contribute to MaskMeister, please submit issues or pull requests on the GitHub repository.
+
+## License
+
+[MIT License](LICENSE)
+
---
+++ src/extension.ts
@@ -0,0 +1,175 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import * as fs from "fs";
+import path = require("path");
+
+export function activate(context: vscode.ExtensionContext) {
+  // Register the maskCode command
+  const maskCodeDisposable = vscode.commands.registerCommand(
+    "masked-code.maskCode",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+
+      const document = editor.document;
+      const originalCode = document.getText();
+      let maskedCode = originalCode;
+
+      const keywordMap = getKeywordMap();
+      for (const [keyword, dummyValue] of keywordMap.entries()) {
+        const regex = new RegExp(`\\b${keyword}\\b`, "gi");
+        maskedCode = maskedCode.replace(regex, dummyValue);
+      }
+
+      editor.edit((editBuilder) => {
+        const fullRange = new vscode.Range(
+          document.positionAt(0),
+          document.positionAt(originalCode.length)
+        );
+        editBuilder.replace(fullRange, maskedCode);
+      });
+    }
+  );
+
+  // Register the unmaskCode command
+  const unmaskCodeDisposable = vscode.commands.registerCommand(
+    "masked-code.unmaskCode",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+
+      const document = editor.document;
+      const maskedCode = document.getText();
+      let originalCode = maskedCode;
+
+      const keywordMap = getKeywordMap();
+      for (const [keyword, dummyValue] of keywordMap.entries()) {
+        const regex = new RegExp(
+          `\\b${dummyValue.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&")}\\b`,
+          "gi"
+        );
+        originalCode = originalCode.replace(regex, keyword);
+      }
+
+      editor.edit((editBuilder) => {
+        const fullRange = new vscode.Range(
+          document.positionAt(0),
+          document.positionAt(maskedCode.length)
+        );
+        editBuilder.replace(fullRange, originalCode);
+      });
+    }
+  );
+
+  // Register the editKeywords command
+  const editKeywordsDisposable = vscode.commands.registerCommand(
+    "masked-code.editKeywords",
+    () => {
+      const keywordsFilePath = vscode.workspace
+        .getConfiguration("masked-code")
+        .get<string>("keywordsFilePath", "keywords.json");
+      vscode.window
+        .showTextDocument(vscode.Uri.file(keywordsFilePath), {
+          viewColumn: vscode.ViewColumn.One,
+          preserveFocus: true,
+          preview: false,
+        })
+        .then((editor) => {
+          editor.edit((editBuilder) => {
+            const document = editor.document;
+            const editRange = new vscode.Range(
+              document.positionAt(0),
+              document.positionAt(document.getText().length)
+            );
+            editBuilder.replace(editRange, "");
+          });
+        });
+    }
+  );
+
+  // Register the code actions provider
+  const codeActionsDisposable = vscode.languages.registerCodeActionsProvider(
+    { scheme: "file", language: "*" },
+    {
+      provideCodeActions(document, range) {
+        const maskAction = new vscode.CodeAction(
+          "Mask code",
+          vscode.CodeActionKind.QuickFix
+        );
+        maskAction.command = {
+          command: "masked-code.maskCode",
+          title: "Mask code",
+          tooltip: "Mask code",
+          arguments: [],
+        };
+
+        const unmaskAction = new vscode.CodeAction(
+          "Unmask code",
+          vscode.CodeActionKind.QuickFix
+        );
+        unmaskAction.command = {
+          command: "masked-code.unmaskCode",
+          title: "Unmask code",
+          tooltip: "Unmask code",
+          arguments: [],
+        };
+
+        return [maskAction, unmaskAction];
+      },
+    }
+  );
+
+  context.subscriptions.push(
+    maskCodeDisposable,
+    unmaskCodeDisposable,
+    editKeywordsDisposable,
+    codeActionsDisposable
+  );
+}
+
+function getKeywordMap(): Map<string, string> {
+  const defaultKeywordMap = new Map([
+    ["password", "****"],
+    ["secret", "**"],
+  ]);
+  const configKeywordMap = vscode.workspace
+    .getConfiguration("masked-code")
+    .get<{ [key: string]: string }>("keywords");
+  const keywordsFilePath = vscode.workspace
+    .getConfiguration("masked-code")
+    .get<string>("keywordsFilePath");
+  let customKeywordMap = new Map();
+  try {
+    let customKeywords;
+    if (keywordsFilePath) {
+      customKeywords = fs.readFileSync(keywordsFilePath, "utf8");
+    } else {
+      const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+      if (!workspaceFolder) {
+        console.error("No workspace folder found.");
+        return defaultKeywordMap;
+      }
+      const filePath = path.join(workspaceFolder.uri.fsPath, "keywords.json");
+      customKeywords = fs.readFileSync(filePath, "utf8");
+    }
+    customKeywordMap = new Map(Object.entries(JSON.parse(customKeywords)));
+  } catch (err) {
+    // Error reading custom keywords file
+    console.error(err);
+  }
+  return new Map([
+    ...defaultKeywordMap,
+    ...(configKeywordMap ? Object.entries(configKeywordMap) : []),
+    ...customKeywordMap,
+  ]);
+}
+
+  
+  
+
+export function deactivate() {}
